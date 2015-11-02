2a51e501d99b83a2631c87d428bb926fa3ba5a1c
pointcha <info@pointcha.ca>
2015-11-02 17:14:01 -0400

    Task 1 complete

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://taskrabbit-rolls.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })     
+      .when('/post', {
+        templateUrl: 'views/post.html',  
+        controller: 'TaskController'      
+      })     
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',  
+        controller: 'TaskController'          
+      })     
+      .when('/browse', {
+        templateUrl: 'views/browse.html',  
+        controller: 'TaskController'          
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
