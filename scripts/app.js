06593fbb88e464f7db9beec66a2c81327179a0de
pointcha <info@pointcha.ca>
2015-11-04 15:34:31 -0400

    Task 2

--- scripts/app.js
+++ scripts/app.js
@@ -2,34 +2,37 @@
 
 var app = angular
   .module('TaskNinjaApp', [
-    'ngAnimate',
-    'ngResource',    
+    'ngAnimate',    
+    'ngResource',
     'ngRoute',    
-    'firebase'
+    'firebase',
+    'toaster'
   ])
   .constant('FURL', 'https://taskrabbit-rolls.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/main.html'        
-      })     
-      .when('/login', {
-        templateUrl: 'views/login.html'   
-      })     
-      .when('/register', {
-        templateUrl: 'views/register.html'   
-      })     
-      .when('/post', {
-        templateUrl: 'views/post.html',  
-        controller: 'TaskController'      
-      })     
-      .when('/edit/:taskId', {
-        templateUrl: 'views/edit.html',  
-        controller: 'TaskController'          
-      })     
+        templateUrl: 'views/main.html'
+      })
       .when('/browse', {
-        templateUrl: 'views/browse.html',  
-        controller: 'TaskController'          
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'     
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthController'
       })
       .otherwise({
         redirectTo: '/'
